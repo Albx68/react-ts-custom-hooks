a30d2341a2797cc8aefa31f2cbf6a7a4182de826
Albert Yumnam <84399334+Albx68@users.noreply.github.com>
2023-09-03 13:16:57 +0530

    Use local storage (#4)
    * useLocalStorage
    
    * added usage

---
+++ src/utils/customHooks/useLocalStorage.ts
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+
+function useLocalStorage<T>(key: string, initialValue: T) {
+    // get stored value from localStorage on initial render
+    const storedValue = localStorage.getItem(key);
+
+    // Initialize the state with the stored value or the provided initial value
+    const [value, setValue] = useState<T>(
+        storedValue ? JSON.parse(storedValue) : initialValue
+    );
+
+    // Update the stored value in localStorage whenever the state changes
+    const setStoredValue = (newValue: T) => {
+        try {
+            setValue(newValue);
+            localStorage.setItem(key, JSON.stringify(newValue));
+        } catch (error) {
+            console.error('Error setting localStorage:', error);
+        }
+    };
+
+    return [value, setStoredValue] as const;
+}
+
+export default useLocalStorage;
